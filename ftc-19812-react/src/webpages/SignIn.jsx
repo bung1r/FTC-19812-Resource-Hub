b51405a421de1d828faf07620375270af5c800d4
Bung1r (Benny) <144744252+bung1r@users.noreply.github.com>
2026-06-29 16:36:49 -0700

    Redirect Fixed
    Should fix google auth

--- ftc-19812-react/src/webpages/SignIn.jsx
+++ ftc-19812-react/src/webpages/SignIn.jsx
@@ -6,6 +6,9 @@ function SignInPage() {
     async function signInWithGoogle() {
         const { error } = await supabase.auth.signInWithOAuth({
             provider: "google",
+            options: {
+                redirectTo: import.meta.env.VITE_APP_URL
+            }
         });
 
         if (error) {
